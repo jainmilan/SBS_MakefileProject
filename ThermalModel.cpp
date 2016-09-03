7c2c04b974e74cd30b006147ca9a68c3a0df83ad
Milan <milanjain81@gmail.com>
2016-09-03 04:23:21 -0400

    Sending actual block instead of error for T_ext

--- ThermalModel.cpp
+++ ThermalModel.cpp
@@ -316,7 +316,7 @@ void ModelRachel::SimulateModel(DF_OUTPUT df[], MAT_FLOAT T_ext_mpc, MAT_FLOAT T
 			//std::cout << T_ext_blk << std::endl;
 			//std::cout << T_ext_eblk << std::endl;
 
-			response = cb.MPCControl(step_size_mpc, time_step_mpc, T_ext_eblk, O_blk, TR2.row(k_spot - 1),
+			response = cb.MPCControl(step_size_mpc, time_step_mpc, T_ext_blk, O_blk, TR2.row(k_spot - 1),
 					DeltaTR1.row(k_spot - 1), ParamsErr, horizon, Time_IH, CV); // Every time step of SPOT except last argument
 			break;
 		default:
